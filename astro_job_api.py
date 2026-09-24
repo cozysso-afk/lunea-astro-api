85e13c3fb0c92adecf7913a401b7805c74532da7
cozysso-afk <cozysso@gmail.com>
2026-09-24 05:40:59 -0700

    Install Vedic Profile V1 routes

--- astro_job_api.py
+++ astro_job_api.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from astro_api import app
 from astro_jobs_v1 import install_astro_jobs
 from profile_auto_api import install_profile_auto
+from vedic_api import install_vedic_api
 
 install_astro_jobs(app)
 install_profile_auto(app)
+install_vedic_api(app)
